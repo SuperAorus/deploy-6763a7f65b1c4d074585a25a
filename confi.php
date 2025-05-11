d3a8c879d76c4710e95755f0ad3a772075889b65
Carlos Rodriguez <105572165+SuperAorus@users.noreply.github.com>
2025-05-11 14:41:25 -0600

    Update confi.php

--- confi.php
+++ confi.php
@@ -5,6 +5,6 @@ define('SMTP_USERNAME', 'waterqualitytampa@outlook.com');
 define('SMTP_PASSWORD', 'Wixxu0-cuvkud-tijxyb');
 define('SMTP_PORT', 587);
 define('SMTP_SECURE', 'tls');
-define('RECIPIENT_EMAIL', 'carlosdrg06@gmail.com');
+define('RECIPIENT_EMAIL', 'ewtcompany@eaglewatertreatments.com');
 define('SENDER_EMAIL', 'waterqualitytampa@outlook.com');
 define('SENDER_NAME', 'Water Quality new contact request');
